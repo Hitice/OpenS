90564b325d677b0a4450501310b0b3d237439037
hitice <adm.nuvem@protonmail.com>
2025-12-01 01:33:01 -0300

    Setup Render

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
-from app import create_app
 import os
+from app import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    # PORT é definido pelo Render
+    # Usa a porta do Render ou 5000 como fallback
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
